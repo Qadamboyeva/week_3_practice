121647d29e0960c16c08f5b21013e66f2348adff
Sevinch Qadamboyeva <sevinchqadamboyeva15@gmail.com>
2025-10-21 12:14:17 +0500

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,15 @@
+print("--- FizzBuzz Challenge (1-100) ---")
+for i in range(1,101): 
+ print(i)
+ if i % 3 == 0:
+   print("Fizz")
+
+ elif i%5==0:
+  print("Buzz")
+
+ elif i%3==0 and i%5==0:
+  print("Fizzbuzz")
+
+else:
+ print("None")
+
